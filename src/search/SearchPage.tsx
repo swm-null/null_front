c34ad5ac34889afde1ade5a1c4bcd60d24d04610
Minji Yun <gee2450@kookmin.ac.kr>
2024-06-19 18:45:03 +0900

    feat: localStorage에 검색 결과 저장하고 불러오는 기능 구현 - 원래 있었던, 중복 데이터 저장 오류 수정

--- src/search/SearchPage.tsx
+++ src/search/SearchPage.tsx
@@ -5,80 +5,95 @@ import { v4 as uuid_v4 } from 'uuid';
 import { useInView } from 'react-intersection-observer';
 
 
-interface View {
+export interface SearchQuery {
   id: string;
-  text: string;
-  left: boolean;
+  query: string;
+  answer: string;
 }
 
 const PAGE_SIZE = 10;
+const MAX_SEARCH_QUERIES = 100;
+const MAX_SEARCH_CHAT = 2 * MAX_SEARCH_QUERIES;
 
 export const SearchPage = () => {
-  const cachedViews = JSON.parse(localStorage.getItem('views') || '[]');
-  const [views, setViews] = useState<View[]>([]);
-  const [page, setPage] = useState(-1);
-  const [isLoading, setIsLoading] = useState(false);
-  const scrollViewRef = useRef<HTMLDivElement | null>(null);
+  // cachedViews를 처음 로드할 때만 설정
+  const [cachedSearchQueries, setCachedSearchQueries] = useState<SearchQuery[]>(() => JSON.parse(localStorage.getItem('search_queries') || '[]'));
+  const [searchQueries, setViews] = useState<SearchQuery[]>([]);
+  const [newSearchQueries, setNewSearchQueries] = useState<SearchQuery[]>([]);
+  const [currentCachedSearchQueriesPage, setCurrentCachedSearchQueriesPage] = useState(-1);
+  const [isCachedSearchQueriesLoading, setIsCachedSearchQueriesLoading] = useState(false);
 
-  const loadViews = async () => {
-    if (page*PAGE_SIZE > cachedViews.length) return;
-
-    const newCachedViews = cachedViews.slice((page+1)*PAGE_SIZE, (page+2)*PAGE_SIZE-1);
-    setViews(prevViews => [...newCachedViews, ...prevViews]);
-    setPage(prevPage => prevPage+1);
+  const canLoadMoreCachedSearchQueries = () => {
+    return (currentCachedSearchQueriesPage+1)*PAGE_SIZE < cachedSearchQueries.length;
+  }
+  
+  const loadCachedSearchQueries = async () => {
+    if (canLoadMoreCachedSearchQueries()) {
+      setCurrentCachedSearchQueriesPage(prevPage => prevPage+1);
+    }
   };
 
-  const handleIntersection = async (inView) => {
-    if (inView && !isLoading) {
-      setIsLoading(true);
-      await loadViews();
-      setIsLoading(false);
+  // scrollView의 끝이 Viewport 안에 들어왔는지 확인하는 메소드
+  const handleIntersection = async (inView: boolean) => {
+    if (inView && !isCachedSearchQueriesLoading) {
+      setIsCachedSearchQueriesLoading(true);
+      await loadCachedSearchQueries();
+      setIsCachedSearchQueriesLoading(false);
     }
   };
 
   // IntersectionObserver hook을 사용하여 빈 view 감지
-  const { ref: emptyViewRef, inView } = useInView({
-    root: scrollViewRef.current,
-    threshold: 0,
-    triggerOnce: false,
+  const { ref: emptyViewRef } = useInView({
     onChange: handleIntersection,
   });
 
-  const addView = (text: string, left: boolean) => {
+  const addSearchQuery = (text: string) => {
     const answerID = uuid_v4();
     
-    setViews((prevViews) => [
-      { id: answerID, text: text, left: left },
+    setNewSearchQueries((prevViews) => [
+      { id: answerID, query: text, answer: '' },
       ...prevViews, 
     ]);
 
     return answerID;
   };
 
-  const editView = (id: string, newText: string) => {
-    setViews(prevViews => prevViews.map(view => view.id === id ? { ...view, text: newText } : view));
+  const editSearchQuery = (id: string, newText: string) => {
+    setNewSearchQueries(prevViews => prevViews.map(view => view.id === id ? { ...view, answer: newText } : view));
+    setCachedSearchQueries(prevViews => prevViews.map(view => view.id === id ? { ...view, answer: newText } : view));
   };
 
-  const removeView = (id: string) => {
-    setViews(prevViews => prevViews.filter(view => view.id !== id));
+  const removeSearchQuery = (id: string) => {
+    setNewSearchQueries(prevViews => prevViews.filter(view => view.id !== id));
+    setCachedSearchQueries(prevViews => prevViews.filter(view => view.id !== id));
     return id;
   };
 
+  // 화면에 보이는 검색 결과 업데이트
+  useEffect(() => {
+    const slicedCachedSearchQueries = cachedSearchQueries.slice(0, Math.min((currentCachedSearchQueriesPage+1)*PAGE_SIZE, cachedSearchQueries.length, MAX_SEARCH_CHAT-newSearchQueries.length));
+    setViews([
+      ...newSearchQueries,
+      ...slicedCachedSearchQueries,
+    ]);
+  }, [newSearchQueries, cachedSearchQueries, currentCachedSearchQueriesPage])
+
+  // 검색 결과 업데이트 될 때마다, localStorage에 저장 (최대 MAX_SEARCH_CHAT개)
   useEffect(() => {
     const temp = [
-      ...views,
-      ...cachedViews,
+      ...newSearchQueries,
+      ...cachedSearchQueries,
     ];
-    localStorage.setItem('views', JSON.stringify(temp.length > 200 ? temp.slice(0, 200) : temp))
-  }, [views])
+    localStorage.setItem('search_queries', JSON.stringify(temp.length > MAX_SEARCH_CHAT ? temp.slice(0, MAX_SEARCH_CHAT) : temp))
+  }, [newSearchQueries, cachedSearchQueries])
 
   return (
     <div className='flex flex-col h-screen'>
       <div className='px-4 py-4'>
         <h1>챗봇으로 메모 검색하기</h1>
       </div>
-      <SearchScrollView ref={scrollViewRef} removeView={removeView} lastElementRef={emptyViewRef} views={views}/>
-      <SearchInput addView={addView} editView={editView}/>
+      <SearchScrollView removeSearchQuery={removeSearchQuery} lastElementRef={emptyViewRef} searchQueries={searchQueries}/>
+      <SearchInput addSearchQuery={addSearchQuery} editSearchQuery={editSearchQuery}/>
     </div>
   );
 };
