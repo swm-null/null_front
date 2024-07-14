3b0beb4e63784af43ec2c7882fd791e544760a12
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-14 22:49:26 +0900

    feat: 모든 메모 보는 api 연동

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
 import { AnimatedHeader, EditableTag } from 'components/ui';
 import useOriginalMemoManager from './hook/useOriginalMemoManager';
-import { dummyData } from './dummyData';
 import { OriginalMemoList } from './components';
 import { Divider } from '@mui/material';
-import { HEADER_ANIMATION_DELAY, HEADER_ANIMATION_DURATION } from 'constants/HeaderSideBarAnimation';
+import {
+  HEADER_ANIMATION_DELAY,
+  HEADER_ANIMATION_DURATION,
+} from 'constants/HeaderSideBarAnimation';
 import { tagInvalidCharsPattern } from 'constants/memo/TagRule';
 
-export const DashboardPage = ({ headerLeftMarginToggle }: { headerLeftMarginToggle?: boolean }) => {
+export const DashboardPage = ({
+  headerLeftMarginToggle,
+}: {
+  headerLeftMarginToggle?: boolean;
+}) => {
   const {
     viewMemos,
     tags,
     updateOriginalMemo,
     deleteOriginalMemo,
-    handleTagClick
-  } = useOriginalMemoManager({dummyData});
-
+    handleTagClick,
+  } = useOriginalMemoManager({});
 
   return (
     <div className="flex flex-col flex-1 h-screen text-gray2">
@@ -27,22 +31,33 @@ export const DashboardPage = ({ headerLeftMarginToggle }: { headerLeftMarginTogg
       />
 
       {/* tag들 선택하는 부분 */}
-      <div className='flex w-full px-4 gap-2 overflow-hidden'>
-        <EditableTag text='모든 메모' invalidCharsPattern={tagInvalidCharsPattern} onClick={() => handleTagClick('all')}/>
-        <Divider orientation='vertical'/>
-        <div className='flex flex-1 overflow-hidden overflow-x-scroll'>
-          <div className='flex flex-none gap-1 overflow-x-scroll'>
-            {
-              tags.map((tag, index) => 
-                <EditableTag text={tag} invalidCharsPattern={tagInvalidCharsPattern} onClick={() => handleTagClick(tag)}/>
-              )
-            }
+      <div className="flex w-full px-4 gap-2 overflow-hidden">
+        <EditableTag
+          text="모든 메모"
+          invalidCharsPattern={tagInvalidCharsPattern}
+          onClick={() => handleTagClick('all')}
+        />
+        <Divider orientation="vertical" />
+        <div className="flex flex-1 overflow-hidden overflow-x-scroll">
+          <div className="flex flex-none gap-1 overflow-x-scroll">
+            {tags.map((tag, index) => (
+              <EditableTag
+                key={index}
+                text={tag}
+                invalidCharsPattern={tagInvalidCharsPattern}
+                onClick={() => handleTagClick(tag)}
+              />
+            ))}
           </div>
         </div>
       </div>
 
       <div className="pb-4 px-4 flex flex-col flex-1 overflow-hidden">
-        <OriginalMemoList originalMemos={viewMemos} updateOriginalMemo={updateOriginalMemo} deleteOriginalMemo={deleteOriginalMemo} />
+        <OriginalMemoList
+          originalMemos={viewMemos}
+          updateOriginalMemo={updateOriginalMemo}
+          deleteOriginalMemo={deleteOriginalMemo}
+        />
       </div>
     </div>
   );
--- src/pages/DashboardPage/hook/useOriginalMemoManager.tsx
+++ src/pages/DashboardPage/hook/useOriginalMemoManager.tsx
@@ -1,37 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Memo } from 'interfaces/MemoInterface';
+import { getAllMemos, isGetAllMemosResponse } from 'utils/auth';
 
-const useOriginalMemoManager = ({ dummyData = [] }: {
-  dummyData?: Memo[]
-}) => {
-  const [originalMemos, setOriginalMemos] = useState<Memo[]>(dummyData || []);
-  const [selectedTag, setSelectedTag] = useState<'all'|string>('all');
-  const viewMemos = selectedTag !== 'all'
-    ? originalMemos.filter((item) => item.tags.includes(selectedTag))
-    : originalMemos;
+const useOriginalMemoManager = ({ dummyData = [] }: { dummyData?: Memo[] }) => {
+  const [originalMemos, setOriginalMemos] = useState<Memo[]>([]);
+  const [selectedTag, setSelectedTag] = useState<'all' | string>('all');
+  const viewMemos =
+    selectedTag !== 'all'
+      ? originalMemos.filter((item) => item.tags.includes(selectedTag))
+      : originalMemos;
 
-  const tags = [...new Set(originalMemos.flatMap((memo, index) => memo.tags))].sort();
+  const tags = [...new Set(originalMemos.flatMap((memo) => memo.tags))].sort();
 
   const updateOriginalMemo = (index: number, newMemo: Memo) => {
-    setOriginalMemos((prev) => prev.map((memo, i) => (i === index ? newMemo : memo)));
+    setOriginalMemos((prev) =>
+      prev.map((memo, i) => (i === index ? newMemo : memo))
+    );
   };
 
   const deleteOriginalMemo = (index: number) => {
-    setOriginalMemos((prev) => prev.filter((memo, i) => i !== index));
+    setOriginalMemos((prev) => prev.filter((_, i) => i !== index));
   };
 
   const handleTagClick = (tag: string) => {
     if (tag !== selectedTag) {
       setSelectedTag(tag);
     }
-  }
+  };
+
+  // FIXME: 현재는 react-query 안 쓰고 대충 서버와 연동만 함. 나중에 추가.
+  const setAllMemos = async () => {
+    const response = await getAllMemos();
+    if (!isGetAllMemosResponse(response)) {
+      setOriginalMemos(dummyData);
+      return;
+    }
+
+    setOriginalMemos(response.memos);
+  };
+
+  useEffect(() => {
+    setAllMemos();
+  }, []);
 
   return {
     viewMemos,
     tags,
     updateOriginalMemo,
     deleteOriginalMemo,
-    handleTagClick
+    handleTagClick,
   };
 };
 
