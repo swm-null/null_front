b0391f5030410f3f61f1b329eb12aa73b3e47d5f
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-14 22:08:27 +0900

    docs: 메모 더미데이터 생성

---
+++ src/pages/DashboardPage/dummyData/index.tsx
@@ -0,0 +1,121 @@
+import { Memo } from "interfaces/MemoInterface";
+
+// Dummy Data
+export const dummyData: Memo[] = [
+  {
+    id: '1',
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Sed sit amet gravida eros. Proin et orci eu nisl sodales ultrices. 
+    Phasellus dapibus congue diam, ut fermentum felis consequat quis. 
+    Integer malesuada dolor a tincidunt tristique. Nulla facilisi. 
+    Sed in purus vel libero ullamcorper volutpat. Nullam laoreet justo 
+    vitae quam rutrum, quis cursus leo suscipit. Pellentesque habitant 
+    morbi tristique senectus et netus et malesuada fames ac turpis egestas.`,
+    tags: ['tag1', 'tag2'],
+  },
+  {
+    id: '2',
+    content: 'Second memo content',
+    tags: ['tag3', 'tag4'],
+  },
+  {
+    id: '3',
+    content: 'Third memo content',
+    tags: ['tag1', 'tag5'],
+  },
+  {
+    id: '4',
+    content: `Suspendisse potenti. In euismod, mi id tincidunt condimentum, 
+    ipsum nunc aliquet eros, id interdum risus magna et neque. 
+    Proin euismod aliquam lectus, sed gravida lectus ultrices eget. 
+    Curabitur lobortis sit amet nisl sit amet vehicula. Fusce convallis 
+    purus et magna pellentesque ultricies. Ut varius felis vel nisi 
+    fringilla, sit amet accumsan est placerat. Nulla facilisi.`,
+    tags: ['tag2', 'tag4'],
+  },
+  {
+    id: '5',
+    content: 'Fifth memo content',
+    tags: ['tag1', 'tag3'],
+  },
+  {
+    id: '6',
+    content: 'Sixth memo content',
+    tags: ['tag2', 'tag5'],
+  },
+  {
+    id: '7',
+    content: 'Seventh memo content',
+    tags: ['tag3', 'tag4'],
+  },
+  {
+    id: '8',
+    content: 'Eighth memo content',
+    tags: ['tag1', 'tag2', 'tag3'],
+  },
+  {
+    id: '9',
+    content: 'Ninth memo content',
+    tags: ['tag4', 'tag5'],
+  },
+  {
+    id: '10',
+    content: `Vestibulum nec ante et nibh lacinia venenatis. 
+    Donec venenatis urna eget elit vestibulum, ut congue sem dictum. 
+    In hac habitasse platea dictumst. Cras a magna metus. 
+    Duis egestas sem at nulla faucibus, at consequat magna placerat. 
+    Duis sed felis vitae eros feugiat feugiat. Duis ut urna mi. 
+    Aenean pretium ultricies arcu, eget pellentesque est condimentum sit amet.`,
+    tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'],
+  },
+  {
+    id: '11',
+    content: 'Eleventh memo content',
+    tags: ['tag2', 'tag3'],
+  },
+  {
+    id: '12',
+    content: 'Twelfth memo content',
+    tags: ['tag1', 'tag4'],
+  },
+  {
+    id: '13',
+    content: 'Thirteenth memo content',
+    tags: ['tag3', 'tag5'],
+  },
+  {
+    id: '14',
+    content: 'Fourteenth memo content',
+    tags: ['tag1', 'tag2'],
+  },
+  {
+    id: '15',
+    content: 'Fifteenth memo content\nFifteenth memo content\nFifteenth memo content\nFifteenth memo content\nFifteenth memo content',
+    tags: ['tag4', 'tag5'],
+  },
+  {
+    id: '16',
+    content: 'Sixteenth memo content',
+    tags: ['tag1', 'tag3'],
+  },
+  {
+    id: '17',
+    content: 'Seventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content\nSeventeenth memo content',
+    tags: ['tag2', 'tag4'],
+  },
+  {
+    id: '18',
+    content: 'Eighteenth memo content',
+    tags: ['tag1', 'tag5'],
+  },
+  {
+    id: '19',
+    content: 'Nineteenth memo content\nNineteenth memo content\nNineteenth memo content\nNineteenth memo content\nNineteenth memo content',
+    tags: ['tag2', 'tag3', 'tag4'],
+  },
+  {
+    id: '20',
+    content: 'Twentieth memo content',
+    tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'],
+  },
+];
