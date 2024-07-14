2161bfacb14ba36f9e9e0d8ee58c365816add6d6
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-14 22:55:03 +0900

    style: 자동배포를 위한 React import 삭제

--- src/pages/DashboardPage/components/OriginalMemoList.tsx
+++ src/pages/DashboardPage/components/OriginalMemoList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { EditableMemo as OriginalMemo } from 'components/memo';
 import { Memo } from 'interfaces/MemoInterface';
 import { Masonry } from '@mui/lab';
@@ -9,12 +8,22 @@ interface OriginalMemoListProps {
   deleteOriginalMemo: (index: number) => void;
 }
 
-export const OriginalMemoList = ({originalMemos, updateOriginalMemo, deleteOriginalMemo}: OriginalMemoListProps) => {
+export const OriginalMemoList = ({
+  originalMemos,
+  updateOriginalMemo,
+  deleteOriginalMemo,
+}: OriginalMemoListProps) => {
   return (
-    <div className='flex flex-col flex-1 overflow-y-scroll no-scrollbar my-4'>
+    <div className="flex flex-col flex-1 overflow-y-scroll no-scrollbar my-4">
       <Masonry columns={{ sm: 1, md: 2, lg: 3 }} spacing={2}>
         {originalMemos.map((memo, index) => (
-          <OriginalMemo editable key={memo.id} memo={memo} updateMemo={(newMemo) => updateOriginalMemo(index, newMemo)} deleteMemo={()=> deleteOriginalMemo(index)}/>
+          <OriginalMemo
+            editable
+            key={memo.id}
+            memo={memo}
+            updateMemo={(newMemo) => updateOriginalMemo(index, newMemo)}
+            deleteMemo={() => deleteOriginalMemo(index)}
+          />
         ))}
       </Masonry>
     </div>
