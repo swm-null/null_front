e12fb6db154bcbf6607c6ef1f7c452ffafcea5f0
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-15 00:18:20 +0900

    hotfix: selelctedTag가 아래에 있어서 나타나는 선언 안 되어서 일어나는 오류 수정 - 코드 위치 수정

--- src/pages/DashboardPage/hook/useOriginalMemoManager.tsx
+++ src/pages/DashboardPage/hook/useOriginalMemoManager.tsx
@@ -3,21 +3,25 @@ import { getAllMemos, isGetAllMemosResponse } from 'utils/auth';
 import { useQuery } from '@tanstack/react-query';
 
 const useOriginalMemoManager = () => {
+  // 선택한 태그
+  const [selectedTag, setSelectedTag] = useState<'all' | string>('all');
+
+  // 메모 전체
   const { data: originalMemosAll = [] } = useQuery({
     queryKey: ['memos', 'all'],
     queryFn: getAllMemos,
     select: (data) => (isGetAllMemosResponse(data) ? data.memos : []),
   });
-
+  // 특정 태그의 메모
   // TODO: Tag를 이용하여 메모를 가져오는 api 만들면, react-query 사용하도록 수정
   const originalMemosByTag = originalMemosAll.filter((item) =>
     item.tags.includes(selectedTag)
   );
-
-  const [selectedTag, setSelectedTag] = useState<'all' | string>('all');
+  // 화면에 보여지는 메모
   const viewMemos =
     selectedTag !== 'all' ? originalMemosByTag : originalMemosAll;
 
+  // 모든 태그
   const tags = [
     ...new Set(originalMemosAll.flatMap((memo) => memo.tags)),
   ].sort();
