5c7c3f5471025637f6bfc4fef123a800103fce13
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-15 18:18:41 +0900

    feat: 화면이 작을 때의 애니메이션 추가 - framer-motion을 이용하여 애니메이션 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import SideBar from './sidebar/SideBar';
 import { SideBarOpenCloseButton } from './sidebar/SideBarOpenCloseButton';
@@ -8,24 +8,64 @@ import { SIDEBAR_HEADER_ANIMATION_DURATION_SECOND } from 'constants/HeaderSideBa
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState<string>('add');
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(
+    window.innerWidth <= 770
+  );
   const [isOpen, setIsOpen] = useState(true);
   const scope = useSideBarOpenCloseButtonAnimation(isOpen);
 
-  const renderContent = () => {
+  const renderContent = (useIsOpen: boolean) => {
     switch (currentPage) {
       case 'add':
-        return <AddPage headerLeftMarginToggle={!isOpen} />;
+        return <AddPage headerLeftMarginToggle={useIsOpen ? !isOpen : true} />;
       case 'search':
-        return <SearchPage headerLeftMarginToggle={!isOpen} />;
+        return (
+          <SearchPage headerLeftMarginToggle={useIsOpen ? !isOpen : true} />
+        );
       default:
-        return <AddPage headerLeftMarginToggle={!isOpen} />;
+        return <AddPage headerLeftMarginToggle={useIsOpen ? !isOpen : true} />;
     }
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 770);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // 화면이 작은 경우
+  if (isSmallScreen) {
+    return (
+      <div ref={scope} className="flex w-full h-full">
+        <SideBarOpenCloseButton handleClick={() => setIsOpen(!isOpen)} />
+        <motion.div
+          className="overflow-x-hidden z-20"
+          animate={{ width: isOpen ? '250px' : '0' }}
+          initial={{ width: '250px' }}
+          transition={{ duration: SIDEBAR_HEADER_ANIMATION_DURATION_SECOND }}
+          style={{ position: 'absolute', left: 0, top: 0, bottom: 0 }}
+        >
+          <SideBar zIndex={20} setCurrentPage={setCurrentPage} />
+        </motion.div>
+        <div
+          className="flex flex-col h-screen overflow-x-hidden z-10 bg-white"
+          style={{ position: 'absolute', right: 0, top: 0, bottom: 0, left: 0 }}
+        >
+          {renderContent(false)}
+        </div>
+      </div>
+    );
+  }
 
   // 화면이 큰 경우, 사이드바를 맨 뒤에 놓고, 페이지 화면의 width를 조절
   return (
     <div ref={scope} className="flex w-full h-full">
-      <SideBar zIndex={10} setCurrentPage={setCurrentPage} />
       <SideBarOpenCloseButton handleClick={() => setIsOpen(!isOpen)} />
       <motion.div
         className="flex flex-col h-screen overflow-x-hidden z-20 bg-white"
@@ -34,8 +74,9 @@ const App = () => {
         transition={{ duration: SIDEBAR_HEADER_ANIMATION_DURATION_SECOND }}
         style={{ position: 'absolute', right: 0, top: 0, bottom: 0 }}
       >
-        {renderContent()}
+        {renderContent(true)}
       </motion.div>
+      <SideBar zIndex={10} setCurrentPage={setCurrentPage} />
     </div>
   );
 };
