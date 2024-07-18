2bb8d2afd3f1173d8a50930c28e6f270f5b7d801
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-18 12:54:52 +0900

    [NULL-160] Feature/mobile sidebar and screen (#23)
    * feat: 사이드바 애니메이션을 없애고, 큰 화면일 경우 페이지의 width를 조절하는 애니메이션 추가
    
    * feat: 안 쓰는 애니메이션 상수 정리와 상수 사용하도록 코드 수정
    
    * feat: 화면이 작을 때의 애니메이션 추가
    - framer-motion을 이용하여 애니메이션 구현
    
    * feat: 작은 화면->큰 화면 전환 자연스럽도록 motion.div의 initial width 수정
    
    * feat: 작은 화면일 때, 사이드 바의 바깥을 클릭할 경우, 사이드 바 닫는 기능 구현
    
    * style: 파라미터, 함수 이름 직관적이게 수정
    - renderContent -> pageContent
    useInOpen -> useHeaderAnimation
    
    * style: 쓸모없는 zIndex 파라미터 삭제
    
    * style: SearchInput, SearchScrollView에 있던, padding 설정 SearchPage로 이동과 수정
    
    * style: 메시지 내부 padding, fontSize 세부 조정
    
    * style: sidebar 버튼 위에 여백 수정
    
    * style: 헤더의 toggleDurationDelay 필요없어짐에 따른 수정
    
    * chore: LeftMessage, RightMessage 파일 이름 변경
    - Left -> AI
    - Right -> User
    
    * chore: processedMessageContent-> answerRelatedMemos로 이름 수정
    
    * chore: header, sidebar 상수를 저장한 파일 이름 수정

--- src/sidebar/SideBar.tsx
+++ src/sidebar/SideBar.tsx
@@ -4,67 +4,45 @@ import { AddIcon, DashboardIcon, SearchIcon } from 'assets/icons';
 
 interface SideBarProps {
   setCurrentPage: (page: string) => void;
-  isSideBarOpen: boolean;
   sideBarWidth?: number;
-  buttonAnimationDuration: number;
-  sideBarAnimationDuration: number;
 }
 
-const SideBar = ({ setCurrentPage, isSideBarOpen, sideBarWidth=250, buttonAnimationDuration, sideBarAnimationDuration }: SideBarProps) => {
-  const buttonControls = useAnimation();
-  const sidebarControls = useAnimation();
-
-  useEffect(() => {
-    if (isSideBarOpen) {
-      sidebarControls.start({ width: sideBarWidth, transition: { duration: sideBarAnimationDuration / 1000 } });
-      setTimeout(() => {
-        buttonControls.start({ opacity: 1, x: 0, transition: { duration: buttonAnimationDuration / 1000 } });
-      }, sideBarAnimationDuration); 
-    } else {
-      buttonControls.start({ opacity: 0, x: -50, transition: { duration: buttonAnimationDuration / 1000 } });
-      setTimeout(() => {
-        sidebarControls.start({ width: 0, transition: { duration: sideBarAnimationDuration / 1000 } });
-      }, buttonAnimationDuration); 
-    }
-  }, [isSideBarOpen, sidebarControls, buttonControls, sideBarWidth, sideBarAnimationDuration, buttonAnimationDuration]);
-
+const SideBar = ({ setCurrentPage, sideBarWidth = 250 }: SideBarProps) => {
   return (
-    <motion.div 
-      className='h-screen bg-gray-100 flex flex-col'
-      animate={sidebarControls}>
+    <div className={`h-screen bg-gray-100 flex flex-col`}>
       {/* TODO: tailwind에서 동적 데이터 지원하는지 확인 */}
-      <div className={`p-4 flex flex-col flex-1`} style={{width: sideBarWidth}}>
+      <div
+        className={`p-4 flex flex-col flex-1`}
+        style={{ width: sideBarWidth }}
+      >
         <div className="items-center my-6">
-          <div className='flex justify-center'>
-            <motion.button 
+          <div className="flex justify-center">
+            <button
               className="my-4 flex py-2 px-6 text-white bg-gray-500 rounded-full"
               onClick={() => setCurrentPage('add')}
-              animate={buttonControls}
-              style={{ opacity: 0, x: -50 }}>
-              <AddIcon className='w-4 h-4 self-center mr-2'/>
+            >
+              <AddIcon className="w-4 h-4 self-center mr-2" />
               Add Memo
-            </motion.button>
+            </button>
           </div>
-          <motion.button 
+          <button
             className="flex items-center p-2 w-full text-left rounded"
             onClick={() => setCurrentPage('search')}
-            animate={buttonControls}
-            style={{ opacity: 0, x: -50 }}>
-            <SearchIcon className='w-5 h-5 self-center mr-2'/>
+          >
+            <SearchIcon className="w-5 h-5 self-center mr-2" />
             Search
-          </motion.button>
-          <motion.button 
+          </button>
+          <button
             className="flex items-center p-2 w-full text-left rounded"
-            onClick={() => setCurrentPage('dashboard')}
-            animate={buttonControls}
-            style={{ opacity: 0, x: -50 }}>
+            onClick={() => setCurrentPage('search')}
+          >
             <DashboardIcon className='w-4 h-4 ml-[2px] self-center mr-[10px]'/>
             Dashboard
-          </motion.button>
+          </button>
         </div>
-        <div className='flex flex-col flex-1'/>
+        <div className="flex flex-col flex-1" />
       </div>
-    </motion.div>
+    </div>
   );
 };
 
--- src/sidebar/SideBarOpenCloseButton.tsx
+++ src/sidebar/SideBarOpenCloseButton.tsx
@@ -9,25 +9,32 @@ const Path = (props: any) => (
 );
 
 // 햄버거 버튼과 메뉴 닫기 버튼
-export const SideBarOpenCloseButton = ({ handleClick }: {handleClick: ()=>void}) => (
-  <button className={`absolute top-2 left-2 rounded-full overflow-hidden grid justify-center items-center`}
-    style={{padding: 5, margin: 7.5}}
-    onClick={handleClick}>
+export const SideBarOpenCloseButton = ({
+  handleClick,
+}: {
+  handleClick: () => void;
+}) => (
+  <button
+    className="z-30 absolute top-[5px] left-2 rounded-full overflow-hidden grid justify-center items-center"
+    style={{ padding: 5, margin: 7.5 }}
+    onClick={handleClick}
+  >
     <svg width="23" height="23" viewBox="-1.5 0 23 18">
       <Path
         d="M 2 2.5 L 20 2.5"
         className="top"
         variants={{
-          closed: { d: "M 2 2.5 L 20 2.5" },
-          open: { d: "M 3 16.5 L 17 2.5" }
-        }}/>
+          closed: { d: 'M 2 2.5 L 20 2.5' },
+          open: { d: 'M 3 16.5 L 17 2.5' },
+        }}
+      />
       <Path d="M 2 9.423 L 20 9.423" opacity="1" className="middle" />
       <Path
         d="M 2 16.346 L 20 16.346"
         className="bottom"
         variants={{
-          closed: { d: "M 2 16.346 L 20 16.346" },
-          open: { d: "M 3 2.5 L 17 16.346" }
+          closed: { d: 'M 2 16.346 L 20 16.346' },
+          open: { d: 'M 3 2.5 L 17 16.346' },
         }}
       />
     </svg>
