99bf0bfb7533d8141d3a58ee5a584c82145aa583
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-18 12:22:15 +0900

    fix: 모든 메모를 보는 기능을 사용할 때, selectedTag를 null로 선택했을 때, 해당 내용을 볼수 있게 수정 - 이전 : selectedTag가 'all'이면 해당 내용을 볼 수 있게 구현했었음

--- src/pages/DashboardPage/hook/useSelectedTagMemosManager.tsx
+++ src/pages/DashboardPage/hook/useSelectedTagMemosManager.tsx
@@ -4,29 +4,34 @@ import { useQuery } from '@tanstack/react-query';
 
 const useSelectedTagMemosManager = () => {
   // 선택한 태그
-  const [selectedTag, setSelectedTag] = useState<'all' | string>('all');
+  // selectedTag가 null일 때에는, 모든 태그가 선택된 경우
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
 
   // 메모 전체
   const { data: allMemos = [], refetch } = useQuery({
-    queryKey: ['memos', 'all'],
+    queryKey: ['memos', 'ALL_MEMOS'],
     queryFn: getAllMemos,
     select: (data) => (isGetAllMemosResponse(data) ? data.memos : []),
   });
   // 특정 태그의 메모
   // TODO: Tag를 이용하여 메모를 가져오는 api 만들면, react-query 사용하도록 수정
   const selectedMemosByTag = allMemos.filter((item) =>
-    item.tags.includes(selectedTag)
+    item.tags.includes(selectedTag || '')
   );
   // 화면에 보여지는 메모
-  const viewMemos = selectedTag !== 'all' ? selectedMemosByTag : allMemos;
+  const viewMemos = selectedTag ? selectedMemosByTag : allMemos;
 
   // 모든 태그
   const tags = [...new Set(allMemos.flatMap((memo) => memo.tags))].sort();
 
-  const handleTagClick = (tag: string) => {
+  const handleTagClick = (tag: string | null) => {
     if (tag !== selectedTag) {
       setSelectedTag(tag);
     }
+  };
+
+  const clickAllTags = () => {
+    handleTagClick(null);
   };
 
   useEffect(() => {
@@ -46,6 +51,7 @@ const useSelectedTagMemosManager = () => {
     viewMemos,
     tags,
     handleTagClick,
+    clickAllTags,
   };
 };
 
