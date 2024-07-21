415b210de0680ddcae5a5bddb75fb7fb5ad9a15b
Minji Yun <gee2450@kookmin.ac.kr>
2024-07-21 12:41:57 +0900

    style: App 파일 생성 후, index.tsx에서는 해당 파일 불러오게 코드 수정

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,1 @@
-import { Home } from 'pages';
-import 'i18n';
-import './index.css';
-
-export const App = () => {
-  // TODO: 로그인, home 라우팅 기능 넣기
-  return <Home />;
-};
+export { App } from './App';
