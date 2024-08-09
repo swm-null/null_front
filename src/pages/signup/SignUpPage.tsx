cf7b57a43462d82edb09f9e0f9e5fd93b825ec7a
Minji Yun <gee2450@kookmin.ac.kr>
2024-08-09 19:47:32 +0900

    style: 회원가입 페이지 컴퍼넌트 분리

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -1,7 +1,19 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { AuthButton, AuthInput } from './components';
+import { useState } from 'react';
 
 const SignUpPage = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleSignUp = () => {
+    navigate(-1);
+  };
+
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [name, setName] = useState('');
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100 py-8">
@@ -28,55 +40,27 @@ const SignUpPage = () => {
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
             />
           </div>
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-700"
-          >
-            {t('signUp.password')}
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+          <AuthInput
+            label={t('signUp.password')}
+            value={password}
+            setValue={setPassword}
+          />
+          <AuthInput
+            label={t('signUp.passwordConfirm')}
+            value={passwordConfirm}
+            setValue={setPasswordConfirm}
           />
         </div>
         <div className="mb-6">
-          <label
-            htmlFor="passwordConfirm"
-            className="block mb-2 text-sm font-medium text-gray-700"
-          >
-            {t('signUp.passwordConfirm')}
-          </label>
-          <input
-            type="password"
-            id="passwordConfirm"
-            name="passwordConfirm"
-            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-          />
+          <AuthInput label={t('signUp.name')} value={name} setValue={setName} />
         </div>
-        <div className="mb-6">
-          <label
-            htmlFor="name"
-            className="block mb-2 text-sm font-medium text-gray-700"
-          >
-            {t('signUp.name')}
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full py-2 mb-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-700"
-        >
-          {t('signUp.signUpButton')}
-        </button>
+        <AuthButton
+          label={t('signUp.signUpButton')}
+          onClick={handleSignUp}
+          bgColor="#3B82F6"
+          hoverColor="#2563EB"
+          additionalClasses="text-white"
+        />
       </form>
     </div>
   );
---
+++ src/pages/signup/components/AuthButton/AuthButton.tsx
@@ -0,0 +1,32 @@
+interface AuthButtonProps {
+  label: string;
+  onClick: () => void;
+  bgColor: string;
+  hoverColor: string;
+  textColor?: string;
+  additionalClasses?: string;
+}
+
+const ActionButton = ({
+  label,
+  onClick,
+  bgColor,
+  hoverColor,
+  textColor = 'white',
+  additionalClasses = '',
+}: AuthButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`w-full py-2 rounded-lg focus:outline-none ${additionalClasses}`}
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onMouseOver={(e) => (e.currentTarget.style.backgroundColor = hoverColor)}
+      onMouseOut={(e) => (e.currentTarget.style.backgroundColor = bgColor)}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ActionButton;
