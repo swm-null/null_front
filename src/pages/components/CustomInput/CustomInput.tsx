ba0b66b1cb443b1a0ff1e89bb84f4a3a612f209c
Minji Yun <gee2450@kookmin.ac.kr>
2024-08-16 00:35:06 +0900

    feat: Custom Input이 hidden Input일 때 내용을 볼 수 있는 토글 추가

--- src/pages/components/CustomInput/CustomInput.tsx
+++ src/pages/components/CustomInput/CustomInput.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { EyeIcon, EyeOffIcon } from 'assets/icons';
+
 const CustomInput = ({
   label,
   value,
@@ -9,17 +12,30 @@ const CustomInput = ({
   setValue: (value: string) => void;
   hidden?: boolean;
 }) => {
+  const [isHidden, setIsHidden] = useState(hidden);
+
+  const toggleHidden = () => {
+    setIsHidden((prev) => !prev);
+  };
+
   return (
-    <>
+    <div className="relative">
       <p className="block mb-2 text-sm font-medium text-gray-700">{label}</p>
-      <input
-        type={hidden ? 'password' : 'text'}
-        id={label}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-      />
-    </>
+      <div className="flex items-center w-full border border-gray-300 rounded-lg py-2 px-2">
+        <input
+          type={isHidden ? 'password' : 'text'}
+          id={label}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="flex-1 focus:outline-none focus:ring focus:border-blue-300"
+        />
+        {hidden && isHidden ? (
+          <EyeIcon width={25} height={25} onClick={toggleHidden} />
+        ) : (
+          <EyeOffIcon width={25} height={25} onClick={toggleHidden} />
+        )}
+      </div>
+    </div>
   );
 };
 
