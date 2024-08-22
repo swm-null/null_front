433141b9d6e6f63f5acb8e7a6cf9e256dc7ad737
Minji Yun <gee2450@kookmin.ac.kr>
2024-08-22 13:47:02 +0900

    style: user message 배경 색 변경

--- src/pages/home/contents/_components/search/SearchConversation/Message/UserMessage.tsx
+++ src/pages/home/contents/_components/search/SearchConversation/Message/UserMessage.tsx
@@ -2,7 +2,7 @@ const UserMessage = ({ contentText }: { contentText: string }) => {
   return (
     <div className="mb-2 flex w-full">
       <div className="flex flex-col flex-1 overflow-clip">
-        <div className="p-3 inline-block self-end bg-gray0 rounded-lg overflow-hidden max-w-3/4">
+        <div className="inline-block self-end bg-[#F4F4F4] rounded-3xl py-2 px-4 overflow-hidden max-w-3/4">
           <p className="inline text-right whitespace-pre-line break-words">
             {contentText}
           </p>
