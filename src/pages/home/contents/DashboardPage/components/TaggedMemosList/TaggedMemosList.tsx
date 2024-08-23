3c2d2b025684871adf853bad6adc03b3105d9d34
Minji Yun <gee2450@kookmin.ac.kr>
2024-08-24 03:10:21 +0900

    fix: taggedMemosList 버튼 안 보이는 오류를 수정

--- src/pages/home/contents/DashboardPage/components/TaggedMemosList/TaggedMemosList.tsx
+++ src/pages/home/contents/DashboardPage/components/TaggedMemosList/TaggedMemosList.tsx
@@ -21,9 +21,9 @@ const TaggedMemosList = ({
   return (
     <div className="w-[360px] flex-shrink-0 py-2">
       <div className="bg-[#989898] rounded-t-2xl flex flex-row justify-between items-stretch overflow-hidden">
-        <div className="flex flex-col pl-3 py-4">
-          <p className="mb-2 text-black">{tag.name}</p>
-          <div className="flex gap-2 overflow-x-scroll w-full no-scrollbar">
+        <div className="grid w-full flex-col px-3 py-4">
+          <p className="mb-2 text-[#3e3e3e]">{tag.name}</p>
+          <div className="flex gap-2 overflow-x-scroll no-scrollbar">
             {childTags.map((childTag, index) => (
               <UneditableTag
                 key={index}
