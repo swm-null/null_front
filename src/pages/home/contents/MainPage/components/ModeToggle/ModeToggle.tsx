1d633a9a7b2526c50def93871a4ce1c6e5fed22d
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-23 13:56:49 +0900

    style: 모드 토글에 아이콘 추가

--- src/pages/home/contents/MainPage/components/ModeToggle/ModeToggle.tsx
+++ src/pages/home/contents/MainPage/components/ModeToggle/ModeToggle.tsx
@@ -1,3 +1,4 @@
+import { AddIcon, SearchIcon } from 'assets/icons';
 import { Mode } from 'pages/home/contents/_interfaces';
 import { useTranslation } from 'react-i18next';
 
@@ -10,14 +11,16 @@ const ModeToggle = ({ mode, onModeChange }: ModeToggleProps) => {
   const { t } = useTranslation();
 
   return (
-    <div className="flex space-x-2 my-4">
+    <div className="flex gap-2 mb-4">
       <ModeToggleButton
         selected={mode === 'search'}
+        icon={<SearchIcon className="w-5 h-5 my-[6px] mr-1" />}
         onModeChange={() => onModeChange('search')}
         text={t('pages.main.mode.search')}
       />
       <ModeToggleButton
         selected={mode === 'create'}
+        icon={<AddIcon className="ml-1 mr-2" />}
         onModeChange={() => onModeChange('create')}
         text={t('pages.main.mode.create')}
       />
@@ -27,20 +30,23 @@ const ModeToggle = ({ mode, onModeChange }: ModeToggleProps) => {
 
 interface ModeToggleButtonProps {
   selected: boolean;
+  icon: React.ReactNode;
   onModeChange: () => void;
   text: string;
 }
 
 const ModeToggleButton = ({
   selected,
+  icon,
   onModeChange,
   text,
 }: ModeToggleButtonProps) => (
   <button
     onClick={onModeChange}
-    className={`rounded-full inline-flex items-center py-2 px-4 border-[1px] border-[#0000001A] cursor-pointer 
+    className={` py-1 px-4 rounded-full inline-flex items-center border-[1px] border-[#E3BFA4] cursor-pointer 
           ${selected ? 'bg-[#FFF6E3] text-[#6A5344]' : 'bg-[#F4CDB1] text-[#846E62]'} `}
   >
+    {icon}
     {text}
   </button>
 );
