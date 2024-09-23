49f56f22afa50b257aec6fa8fd7e1c6bf6dcc9b2
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-23 13:22:22 +0900

    style: 이미지 아이콘으로 변경

--- src/pages/home/contents/MainPage/components/MemoTextAreaWithMicAndCameraButton/MemoTextAreaWithMicAndCameraButton.tsx
+++ src/pages/home/contents/MainPage/components/MemoTextAreaWithMicAndCameraButton/MemoTextAreaWithMicAndCameraButton.tsx
@@ -1,8 +1,7 @@
 import { ChangeEvent } from 'react';
 import { TextareaAutosize } from '@mui/material';
 import { usePressEnterFetch } from './hook';
-import { SearchIcon } from 'assets/icons';
-import { cameraUrl, micUrl } from 'assets/images';
+import { CameraIcon, MicIcon, SearchIcon } from 'assets/icons';
 
 interface MemoTextAreaWithMicAndCameraButtonProps {
   value: string;
@@ -26,8 +25,8 @@ const MemoTextAreaWithMicAndCameraButton = ({
   });
 
   return (
-    <div className="flex flex-shrink-0 items-start rounded-3xl border-[0.12rem] px-4 py-2 overflow-hidden">
-      <SearchIcon className="w-5 h-5 mt-[2px]" />
+    <div className="flex flex-shrink-0 items-center rounded-3xl bg-[#FFF6E3CC] border-[1px] border-[#0000001A] px-4 py-3 overflow-hidden ">
+      <SearchIcon />
       <TextareaAutosize
         value={value}
         onChange={onChange}
@@ -35,19 +34,10 @@ const MemoTextAreaWithMicAndCameraButton = ({
         onKeyDown={handlePressEnterFetch}
         minRows={1}
         maxRows={20}
-        className="flex-1 flex-shrink-0 focus:outline-none resize-none mr-4 ml-2"
+        className="flex-1 flex-shrink-0 focus:outline-none resize-none mr-4 ml-2 bg-transparent min-h-9 content-center"
       />
-
-      <img
-        src={micUrl}
-        className="w-6 h-6 mr-1 cursor-pointer"
-        onClick={onMicButtonClick}
-      />
-      <img
-        src={cameraUrl}
-        className="w-6 h-6 cursor-pointer"
-        onClick={onCameraButtonClick}
-      />
+      <MicIcon className="cursor-pointer mr-1" onClick={onMicButtonClick} />
+      <CameraIcon className="cursor-pointer" onClick={onCameraButtonClick} />
     </div>
   );
 };
