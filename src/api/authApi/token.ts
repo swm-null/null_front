f27d0bc7abf8cc00340abcfe1f3d77acbae5102f
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-26 18:48:42 +0900

    feat: refresh 구현

---
+++ src/api/authApi/token.ts
@@ -0,0 +1,44 @@
+import { errorResponse } from 'api/interface';
+import { API_BASE_URL, errorHandler } from 'api/utils';
+import axios from 'axios';
+import saveToken from './saveToken';
+
+interface tokenResponse {
+  access_token: string;
+  refresh_token: string;
+}
+
+export const refresh = async (
+  refresh_token: string
+): Promise<tokenResponse | errorResponse> => {
+  const method = 'login';
+  const endpoint = `${API_BASE_URL}/user/refresh`;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const response = await axios.post(
+      endpoint,
+      JSON.stringify({ refresh_token }),
+      config
+    );
+    const responseInfo = {
+      method,
+      status: response.status,
+      access_token: response.data.access_token,
+      refresh_token: response.data.refresh_token,
+    } as tokenResponse;
+    saveToken(responseInfo.access_token, responseInfo.refresh_token);
+    return responseInfo;
+  } catch (error) {
+    return errorHandler(error, method);
+  }
+};
+
+export const isTokenResponse = (
+  response: tokenResponse | errorResponse
+): response is tokenResponse => {
+  return (response as tokenResponse).access_token !== undefined;
+};
