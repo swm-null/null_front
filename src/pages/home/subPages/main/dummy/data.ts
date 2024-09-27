34f5e98bd9647faeeb2435be919744c60110683c
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-27 23:56:39 +0900

    style: test/dummyData 파일 -> dummy/data 파일로 이름, 폴더 변경

---
+++ src/pages/home/subPages/main/dummy/data.ts
@@ -0,0 +1,72 @@
+// TODO: 삭제 예정(api 구현 되어있음)
+export const memos = [
+  {
+    id: '1',
+    content: 'test',
+    image_urls: [],
+    updated_at: '2024-09-08T11:08:24.189Z',
+    created_at: '2024-09-08T11:08:24.189Z',
+    tags: [
+      {
+        id: '60c72b3e9b1e8b1e4c8b4568',
+        name: '일정',
+      },
+    ],
+  },
+  {
+    id: '2',
+    content: 'another test',
+    image_urls: [],
+    updated_at: '2024-09-08T12:15:24.189Z',
+    created_at: '2024-09-08T12:15:24.189Z',
+    tags: [
+      {
+        id: '60c72b3e9b1e8b1e4c8b4568',
+        name: '일정',
+      },
+    ],
+  },
+  {
+    id: '3',
+    content: 'memo example',
+    image_urls: ['https://example.com/image1.jpg'],
+    updated_at: '2024-09-08T13:45:24.189Z',
+    created_at: '2024-09-08T13:45:24.189Z',
+    tags: [
+      {
+        id: '60c72b3e9b1e8b1e4c8b4568',
+        name: '일정',
+      },
+      {
+        id: '60d72b4e9b1e8b1e4c8b1234',
+        name: '업무',
+      },
+    ],
+  },
+  {
+    id: '4',
+    content: 'example content',
+    image_urls: [],
+    updated_at: '2024-09-08T14:10:24.189Z',
+    created_at: '2024-09-08T14:10:24.189Z',
+    tags: [
+      {
+        id: '60c72b3e9b1e8b1e4c8b4568',
+        name: '일정',
+      },
+    ],
+  },
+  {
+    id: '5',
+    content: 'last test',
+    image_urls: ['https://example.com/image2.jpg'],
+    updated_at: '2024-09-08T15:30:24.189Z',
+    created_at: '2024-09-08T15:30:24.189Z',
+    tags: [
+      {
+        id: '60c72b3e9b1e8b1e4c8b4568',
+        name: '일정',
+      },
+    ],
+  },
+];
