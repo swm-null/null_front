3fece4ec925637e3a4e60f90a172b1d762f63ce2
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-29 17:30:37 +0900

    feat: Home에서 로그인 알림창이 뜨는 기능 구현 - Home에 처음 들어갈때, 액세스 토큰이 만료 되었으면, 만료 알림 생성 - 액세스 토큰이 없으면, 로그인이 필요하다는 알림 생성

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,53 @@
+import { useContext, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Cookies from 'js-cookie';
 import { ResponsiveLayout } from './components';
 import { useNavigate } from 'react-router-dom';
 import { HomeRouter } from './router';
+import { jwtDecode } from 'jwt-decode';
+import { AlertContext } from 'utils/context';
+import { useTranslation } from 'react-i18next';
 
 const queryClient = new QueryClient();
 
 const Home = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
+  const { alert } = useContext(AlertContext);
+
+  const onAlertClick = async (message: string, page: string) => {
+    await alert(message);
+    handleNavigation(page);
+  };
 
   const handleNavigation = (page: string) => {
     navigate(`/${page}`);
   };
+
+  useEffect(() => {
+    const accessToken = Cookies.get('access_token');
+
+    interface TokenPayload {
+      exp: number;
+      iat: number;
+    }
+
+    const isTokenValid = (token: string) => {
+      if (!token) return false;
+
+      const decoded = jwtDecode<TokenPayload>(token);
+      const currentTime = Date.now() / 1000;
+
+      return decoded.exp > currentTime;
+    };
+
+    if (!accessToken) {
+      onAlertClick(t('utils.auth.loginRequired'), 'login');
+    } else if (!isTokenValid(accessToken)) {
+      onAlertClick(t('utils.auth.sessionExpired'), 'login');
+    }
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
