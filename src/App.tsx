5ab66571c932d4afaaf158fd71211a6392e24463
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-30 16:15:08 +0900

    feat: provider 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'pages';
+import { AlertProvider, ApiProvider } from 'utils/provider';
 import 'i18n';
 import './index.css';
-import { Router } from 'pages';
-import { AlertDialog } from 'utils/dialog';
 
 const App = () => {
   return (
-    <AlertDialog>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </AlertDialog>
+    <BrowserRouter>
+      <AlertProvider>
+        <ApiProvider>
+          <Router />
+        </ApiProvider>
+      </AlertProvider>
+    </BrowserRouter>
   );
 };
 
