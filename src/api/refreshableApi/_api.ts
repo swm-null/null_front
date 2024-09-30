3b8b33156caba5982511a3a2c77e5e49145e98bf
Minji Yun <gee2450@kookmin.ac.kr>
2024-09-30 19:20:05 +0900

    fix: header에 액세스 토큰을 넣는 것을 Interceptor에서 수행과 코드 정리 - _api에 create하며 설정했던 액세스 토큰 설정을 ApiProvider에서 interceptor로 수행

--- src/api/refreshableApi/_api.ts
+++ src/api/refreshableApi/_api.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 import { API_BASE_URL } from 'api/utils';
 
 export const refreshableApi = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    Authorization: `Bearer ${Cookies.get('access_token')}`,
     'Content-Type': 'application/json',
   },
 });
--- src/utils/provider/ApiProvider.tsx
+++ src/utils/provider/ApiProvider.tsx
@@ -1,55 +1,91 @@
-import axios from 'axios';
 import Cookies from 'js-cookie';
-import { useContext, ReactNode } from 'react';
+import { useContext, ReactNode, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AlertContext, ApiContext } from 'utils/context';
 import { isTokenResponse, refresh } from 'api/authApi/token';
 import { authApi, refreshableApi } from 'api';
 import { useNavigate } from 'react-router-dom';
+import { AlertDialog } from 'utils/dialog';
 
 const ApiProvider = ({ children }: { children: ReactNode }) => {
-  let isRefreshing = false;
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const { alert } = useContext(AlertContext);
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  let refreshSubscribers: Array<(token: string) => void> = [];
+
+  const onAccessTokenFetched = (accessToken: string) => {
+    refreshSubscribers.forEach((callback) => callback(accessToken));
+    refreshSubscribers = [];
+  };
+
+  const addRefreshSubscriber = (callback: (token: string) => void) => {
+    refreshSubscribers.push(callback);
+  };
+
+  refreshableApi.interceptors.request.use((config) => {
+    const accessToken = Cookies.get('access_token');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  });
+
   refreshableApi.interceptors.response.use(undefined, async (error) => {
+    const errorStatus = error.response.status;
+    const originalRequest = error.config;
+    const refresh_token = Cookies.get('refresh_token');
+
     const isAccessTokenExpired = (status: number) => status === 401;
     const isRefreshTokenExpired = (status: number) => status === 403;
 
-    const errorStatus = error.response.status;
-    if (isAccessTokenExpired(errorStatus)) {
-      const refresh_token = Cookies.get('refresh_token');
-      if (refresh_token) {
-        if (!isRefreshing) {
-          isRefreshing = true;
-          try {
-            console.log('refresh try');
-            const response = await refresh(refresh_token);
-            if (isTokenResponse(response)) {
-              const newAccessToken = response.access_token;
-              refreshableApi.defaults.headers.common['Authorization'] =
-                `Bearer ${newAccessToken}`;
-              return axios(error.config);
-            }
-          } catch (error) {
-            return Promise.reject(error);
-          } finally {
-            isRefreshing = false;
-          }
-        }
-      }
+    if (isAccessTokenExpired(errorStatus) && refresh_token) {
+      return handleAccessTokenExpiration(originalRequest, refresh_token);
     } else if (isRefreshTokenExpired(errorStatus)) {
-      console.log('refresh token expired');
-      const { alert } = useContext(AlertContext);
-      const { t } = useTranslation();
-      const navigate = useNavigate();
-      alert(t('utils.auth.sessionExpired')).then(() => {
-        navigate('login');
+      alert(t('utils.auth.sessionExpired')).then(() => navigate('login'));
+    } else {
+      return Promise.reject(error);
+    }
+  });
+
+  const handleAccessTokenExpiration = async (
+    originalRequest: any,
+    refresh_token: string
+  ) => {
+    if (isRefreshing) {
+      return new Promise((resolve) => {
+        addRefreshSubscriber((newToken) => {
+          originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+          resolve(refreshableApi(originalRequest));
+        });
       });
     }
-    return Promise.reject(error);
-  });
+
+    setIsRefreshing(true);
+    try {
+      const response = await refresh(refresh_token);
+      if (isTokenResponse(response)) {
+        const newAccessToken = response.access_token;
+        refreshableApi.defaults.headers.common['Authorization'] =
+          `Bearer ${newAccessToken}`;
+        onAccessTokenFetched(newAccessToken);
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+        return refreshableApi(originalRequest);
+      } else {
+        throw new Error('Invalid token response');
+      }
+    } catch (error) {
+      alert(t('utils.auth.sessionExpired')).then(() => navigate('login'));
+      return Promise.reject(error);
+    } finally {
+      setIsRefreshing(false);
+    }
+  };
 
   return (
     <ApiContext.Provider value={{ authApi, refreshableApi }}>
       {children}
+      <AlertDialog />
     </ApiContext.Provider>
   );
 };
