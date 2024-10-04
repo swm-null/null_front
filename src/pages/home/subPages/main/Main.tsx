91ad92f65777ab719285681bd4a146fa15569fed
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-04 17:58:18 +0900

    style: TODO 주석 실수로 삭제한거 복구

--- src/pages/home/subPages/main/Main.tsx
+++ src/pages/home/subPages/main/Main.tsx
@@ -42,6 +42,7 @@ const MainPage = ({ navigateToHistory }: { navigateToHistory: () => void }) => {
     // TODO: camera OCR 기능이 추가되면 코드 작성하기
   };
 
+  // TODO: 나중에는 서버에서 요청해서 가져올 예정
   const buttonData: [string, string, string, string] = [
     '라면 레시피 메모 보여줘',
     '민지 전화번호 알려줘',
@@ -82,6 +83,8 @@ const MainPage = ({ navigateToHistory }: { navigateToHistory: () => void }) => {
   );
 
   return (
+    // FIXME: pt 지금은 그냥 직접 구해서 넣었는데, 나중에 어떻게 하면 저거 자동으로 구할지 상의해보기
+    // 50vh - (토글+여백+TextArea 기본 높이) - 사이드바 절반 높이
     <div className="bg-custom-gradient-basic pt-[calc(50vh-120px-140px)] pb-14 px-4 flex h-full justify-center">
       <div className="max-w-[740px] flex flex-col flex-1 text-gray3">
         <Component.ModeToggle mode={mode} onModeChange={handleModeChange} />
