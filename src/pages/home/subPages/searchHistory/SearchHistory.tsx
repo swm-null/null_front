5cc5b4a8955f48e9af95b2ae40560bd35f9b532c
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-04 16:29:55 +0900

    feat: 검색 입력창 추가

--- src/pages/home/subPages/searchHistory/SearchHistory.tsx
+++ src/pages/home/subPages/searchHistory/SearchHistory.tsx
@@ -3,24 +3,41 @@ import { Header } from 'pages/home/subPages/components';
 import { MemoSearchConversation } from 'pages/home/subPages/interfaces';
 import { SearchScrollView } from './components';
 import { SearchHistoryAccordion } from './components/SearchHistoryAccordion';
+import { MemoSearchTextArea } from '../components/memo/MemoSearchTextArea';
+import { ChangeEvent, useState } from 'react';
 
 const SearchHistoryPage = ({}: {}) => {
   const { t } = useTranslation();
+  const [message, setMessage] = useState('');
   const searchConversations: MemoSearchConversation[] = JSON.parse(
     localStorage.getItem('search_queries') || '[]'
   );
+  const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+  const handleSubmit = (message: string) => {
+    console.log(message);
+  };
 
   return (
-    <div className="flex flex-col flex-1 h-screen bg-custom-gradient-basic text-gray2 px-4 pb-4">
-      <Header headerText={t('pages.search.header')} />
-      <div className="max-w-[740px] w-full flex flex-col flex-1 text-gray2 overflow-hidden self-center">
+    <div className="flex flex-col flex-1 h-screen bg-custom-gradient-basic">
+      <div className="max-w-[740px] pt-24 pb-16 w-full flex flex-col flex-1 text-gray2 overflow-hidden self-center">
+        <Header headerText={t('pages.searchHistory.header')} />
         <SearchScrollView searchConversations={searchConversations}>
-          {searchConversations.map((searchConversation) => (
-            <SearchHistoryAccordion
-              key={searchConversation.id}
-              data={searchConversation}
+          <>
+            <MemoSearchTextArea
+              value={message}
+              onChange={handleMessageChange}
+              placeholder={t('pages.searchHistory.inputPlaceholder')}
+              onSubmit={() => handleSubmit(message)}
             />
-          ))}
+            {searchConversations.map((searchConversation) => (
+              <SearchHistoryAccordion
+                key={searchConversation.id}
+                data={searchConversation}
+              />
+            ))}
+          </>
         </SearchScrollView>
       </div>
     </div>
