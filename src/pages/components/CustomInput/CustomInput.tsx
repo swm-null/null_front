3f020a83c758b8df644041101b300675ffb30c3a
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-07 02:21:46 +0900

    style: icon 색 적용

--- src/pages/components/CustomInput/CustomInput.tsx
+++ src/pages/components/CustomInput/CustomInput.tsx
@@ -23,21 +23,31 @@ const CustomInput = ({
   };
 
   return (
-    <div className="relative mb-4">
+    <div className="relative w-full">
       <p className="block mb-2 text-sm font-medium text-gray-700">{label}</p>
-      <div className="flex items-center w-full border border-gray-300 rounded-lg py-2 px-2">
+      <div className="flex items-center w-full border bg-white border-gray-300 rounded-lg py-2 px-2">
         <input
           type={isHidden ? 'password' : 'text'}
           id={label}
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          className="flex-1 focus:outline-none focus:ring focus:border-blue-300"
+          className="flex-1 w-full focus:outline-none focus:ring focus:border-blue-300"
         />
         {useHiddenToggle &&
           (isHidden ? (
-            <EyeIcon width={25} height={25} onClick={toggleHidden} />
+            <EyeIcon
+              className="text-brown0 text-brown2"
+              width={25}
+              height={25}
+              onClick={toggleHidden}
+            />
           ) : (
-            <EyeOffIcon width={25} height={25} onClick={toggleHidden} />
+            <EyeOffIcon
+              className="text-brown0 text-brown2"
+              width={25}
+              height={25}
+              onClick={toggleHidden}
+            />
           ))}
       </div>
       {errorMessage && (
