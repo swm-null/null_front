167d8ebf303deddef8f79030cc1edd23b5e1f3c6
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-07 16:02:53 +0900

    feat: profile button 생성 후 api 연동

--- src/pages/home/components/ResponsiveLayout/MobileLayout/MobileLayout.tsx
+++ src/pages/home/components/ResponsiveLayout/MobileLayout/MobileLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { BottomNavBar } from './BottomNavBar';
+import { ProfileButton } from '../ProfileButton';
 
 interface MobileLayoutProps {
   setCurrentPage: (page: string) => void;
@@ -9,7 +10,12 @@ interface MobileLayoutProps {
 const MobileLayout = ({ setCurrentPage, children }: MobileLayoutProps) => {
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex-grow overflow-y-auto">{children}</div>
+      <div className="flex-grow overflow-y-auto">
+        <div className="absolute top-0 right-0 m-4 z-30">
+          <ProfileButton />
+        </div>
+        {children}
+      </div>
       <BottomNavBar setCurrentPage={setCurrentPage} />
     </div>
   );
