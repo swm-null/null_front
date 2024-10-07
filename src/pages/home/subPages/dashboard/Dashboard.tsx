be87232a120ee2451a6e5fde4bd4478690f12552
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-07 15:26:10 +0900

    fix: rebase 실수

--- src/pages/home/subPages/dashboard/Dashboard.tsx
+++ src/pages/home/subPages/dashboard/Dashboard.tsx
@@ -43,7 +43,7 @@ const DashboardPage = ({}: {}) => {
     <div className="flex flex-col h-full bg-custom-gradient-basic text-gray2 overflow-hidden px-4 pb-4">
       <div className="w-full h-full pt-12 pb-8 flex flex-col max-w-[1102px] self-center gap-4">
         <Header headerText={t('pages.dashboard.header')} />
-        <div className="flex flex-col gap-[0.9rem] overflow-hidden">
+        <div className="flex flex-col flex-1 gap-[0.9rem] overflow-hidden">
           <Components.CurrentTagPath
             allTagText={t('pages.dashboard.allMemoButton')}
             tags={tagsManager.tags}
