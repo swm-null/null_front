4549699a39529a7eb4d1f0b04f68e47e145e8a54
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-15 00:49:19 +0900

    hotfix: 검색 기록에서 줄넘김 적용되게 수정

--- src/pages/home/subPages/searchHistory/components/SearchHistoryAccordion/AccordionContent/AccordionContent.tsx
+++ src/pages/home/subPages/searchHistory/components/SearchHistoryAccordion/AccordionContent/AccordionContent.tsx
@@ -9,28 +9,30 @@ const AccordionContent = ({
   isOpen: boolean;
   contentRef: React.RefObject<HTMLDivElement>;
   data: MemoSearchConversation;
-}) => (
-  <div
-    ref={contentRef}
-    style={{
-      maxHeight: isOpen ? `${contentRef.current?.scrollHeight}px` : '0px',
-      overflow: 'hidden',
-      transition: 'max-height 0.3s ease',
-    }}
-  >
-    <div className="flex flex-col flex-1 gap-4 px-5 pb-5">
-      <p className="pr-2 pl-9 font-regular text-brown2">
-        {data.search_memos_response?.processed_message}
-      </p>
-      {data.search_memos_response?.memos?.length !== 0 && (
-        <MemosList>
-          {data.search_memos_response?.memos?.map((memo) => (
-            <UneditableMemo key={memo.id} memo={memo} />
-          ))}
-        </MemosList>
-      )}
+}) => {
+  return (
+    <div
+      ref={contentRef}
+      style={{
+        maxHeight: isOpen ? `${contentRef.current?.scrollHeight}px` : '0px',
+        overflow: 'hidden',
+        transition: 'max-height 0.3s ease',
+      }}
+    >
+      <div className="flex flex-col flex-1 gap-4 px-5 pb-5">
+        <p className="pr-2 pl-9 font-regular text-brown2 whitespace-break-spaces">
+          {data.search_memos_response?.processed_message}
+        </p>
+        {data.search_memos_response?.memos?.length !== 0 && (
+          <MemosList>
+            {data.search_memos_response?.memos?.map((memo) => (
+              <UneditableMemo key={memo.id} memo={memo} />
+            ))}
+          </MemosList>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AccordionContent;
