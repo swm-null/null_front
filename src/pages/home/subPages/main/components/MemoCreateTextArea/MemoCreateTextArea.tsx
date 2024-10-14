4c557e24653f97e56d9a259eeee8843be681ceb7
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-15 05:06:33 +0900

    feat: textarea 줄이 길어질 때, 버튼이 오른쪽 상단에서 오른쪽 하단으로 내려가도록 focus, blur, tabIndex를 이용하여 구현

--- src/pages/home/subPages/main/components/MemoCreateTextArea/MemoCreateTextArea.tsx
+++ src/pages/home/subPages/main/components/MemoCreateTextArea/MemoCreateTextArea.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { TextareaAutosize } from '@mui/material';
-import { CameraIcon, MicIcon } from 'assets/icons';
+import { CameraIcon, MicIcon, RightArrowIcon } from 'assets/icons';
 import { usePressEnterFetch } from 'pages/home/subPages/hooks';
 
 interface MemoCreateTextAreaProps {
@@ -20,44 +20,139 @@ const MemoCreateTextArea = ({
   onMicButtonClick,
   onCameraButtonClick,
 }: MemoCreateTextAreaProps) => {
-  const { handlePressEnterFetch } = usePressEnterFetch({
-    handleCtrlSubmit: onSubmit,
-  });
   const [focus, setFocus] = useState(false);
-  const getBackgroundColor = () => {
-    if (focus) {
-      return 'bg-[#FFF6E3]';
-    } else {
-      return 'bg-[#FFF6E3CC]';
-    }
+  const handleSubmit = () => {
+    setFocus(false);
+    onSubmit();
   };
+  const { handlePressEnterFetch } = usePressEnterFetch({
+    handleCtrlSubmit: handleSubmit,
+  });
+
+  const [isMultiline, setIsMultiline] = useState(false);
+  const [hiddenTextareaWidth, setHiddenTextareaWidth] = useState<number | null>(
+    null
+  );
+
+  const hiddenTextareaRef = useRef<HTMLTextAreaElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (hiddenTextareaRef.current && containerRef.current) {
+      const lineHeight = parseFloat(
+        getComputedStyle(hiddenTextareaRef.current).lineHeight
+      );
+      const maxLines = Math.floor(
+        hiddenTextareaRef.current.clientHeight / lineHeight
+      );
+      setIsMultiline(maxLines > 1);
+      setHiddenTextareaWidth(containerRef.current.clientWidth);
+    }
+  }, [value]);
 
   return (
     <div
-      className={`flex flex-shrink-0 px-4 py-3 items-start rounded-2xl overflow-hidden gap-4
-        ${getBackgroundColor()} border-[1px] border-black border-opacity-10 bg-clip-padding 
-        font-regular shadow-custom backdrop-blur-lg`}
+      className={`flex flex-shrink-0 px-4 py-3 rounded-2xl overflow-hidden gap-4 
+        bg-[#FFF6E3CC] border-[1px] border-black border-opacity-10 font-regular shadow-custom backdrop-blur-lg`}
+      onFocus={() => setFocus(true)}
+      onBlur={(e) => {
+        if (
+          containerRef.current &&
+          !containerRef.current.contains(e.relatedTarget)
+        ) {
+          setFocus(false);
+        }
+      }}
     >
-      <div className="flex flex-1 gap-2">
+      <HiddenTextarea
+        value={value}
+        hiddenTextareaWidth={hiddenTextareaWidth}
+        hiddenTextareaRef={hiddenTextareaRef}
+      />
+
+      <div
+        ref={containerRef}
+        className={`flex flex-1 ${isMultiline ? 'flex-col' : 'flex-row'}`}
+      >
         <TextareaAutosize
-          className="flex-1 flex-shrink-0 focus:outline-none resize-none min-h-9 content-center 
+          className="flex-auto focus:outline-none resize-none min-h-9 content-center 
             text-[#111111] bg-transparent placeholder-custom"
           value={value}
           onChange={onChange}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
           placeholder={placeholder}
           onKeyDown={handlePressEnterFetch}
           minRows={1}
           maxRows={20}
         />
-      </div>
-      <div className="flex gap-1">
-        <MicIcon className="cursor-pointer" onClick={onMicButtonClick} />
-        <CameraIcon className="cursor-pointer" onClick={onCameraButtonClick} />
+        <IconButtons
+          focus={focus}
+          onMicButtonClick={onMicButtonClick}
+          onCameraButtonClick={onCameraButtonClick}
+          onSubmitButtonClick={handleSubmit}
+        />
       </div>
     </div>
   );
 };
+
+const HiddenTextarea = ({
+  value,
+  hiddenTextareaWidth,
+  hiddenTextareaRef,
+}: {
+  value: string;
+  hiddenTextareaWidth: number | null;
+  hiddenTextareaRef: React.RefObject<HTMLTextAreaElement>;
+}) => (
+  <div
+    className="flex flex-1"
+    style={{
+      position: 'absolute',
+      top: '9999px',
+      width: hiddenTextareaWidth ? `${hiddenTextareaWidth}px` : 'auto',
+    }}
+  >
+    <TextareaAutosize
+      className="flex-1 focus:outline-none resize-none min-h-9 content-center 
+        text-[#111111] bg-transparent placeholder-custom"
+      value={value}
+      ref={hiddenTextareaRef}
+    />
+  </div>
+);
+
+const IconButtons = ({
+  focus,
+  onMicButtonClick,
+  onCameraButtonClick,
+  onSubmitButtonClick,
+}: {
+  focus: boolean;
+  onMicButtonClick?: () => void;
+  onCameraButtonClick?: () => void;
+  onSubmitButtonClick?: () => void;
+}) => (
+  <div className="flex justify-end gap-1 items-center">
+    <MicIcon
+      tabIndex={0}
+      className="w-7 h-7 cursor-pointer"
+      onClick={onMicButtonClick}
+    />
+    <CameraIcon
+      tabIndex={0}
+      className="w-7 h-7 cursor-pointer"
+      onClick={onCameraButtonClick}
+    />
+    {focus && (
+      <RightArrowIcon
+        tabIndex={0}
+        className="w-7 h-7 mx-1 p-1 cursor-pointer -rotate-90 rounded-full bg-[#F4CDB1]"
+        onClick={() => {
+          onSubmitButtonClick && onSubmitButtonClick();
+        }}
+      />
+    )}
+  </div>
+);
 
 export default MemoCreateTextArea;
