b67dc296e4b242f1d61d9baf9d26f974a20a3ab7
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-20 15:04:26 +0900

    design: search history 에서 query 가 길어질 경우 깨지는 디자인 수정 - accordion이 닫혀있을 경우 ... 으로 요약 보여줌 - accordion이 열려있을 경우 전체 query 보여주는 대신 date를 content로 이동

--- src/pages/home/subPages/searchHistory/components/SearchHistoryAccordion/AccordionContent/AccordionContent.tsx
+++ src/pages/home/subPages/searchHistory/components/SearchHistoryAccordion/AccordionContent/AccordionContent.tsx
@@ -1,26 +1,37 @@
 import { MemosList, UneditableMemo } from 'pages/home/subPages/components';
 import { MemoSearchConversation } from 'pages/home/subPages/interfaces';
+import { useEffect, useState } from 'react';
 
 const AccordionContent = ({
   isOpen,
   contentRef,
   data,
+  formatDate,
 }: {
   isOpen: boolean;
   contentRef: React.RefObject<HTMLDivElement>;
   data: MemoSearchConversation;
+  formatDate: (date: string) => string;
 }) => {
+  const [accordionHeight, setAccordionHeight] = useState(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setAccordionHeight(contentRef.current.scrollHeight);
+    }
+  }, [contentRef.current?.scrollHeight]);
+
   return (
     <div
       ref={contentRef}
       style={{
-        maxHeight: isOpen ? `${contentRef.current?.scrollHeight}px` : '0px',
+        maxHeight: isOpen ? `${accordionHeight}px` : '0px',
         overflow: 'hidden',
         transition: 'max-height 0.3s ease',
       }}
     >
       <div className="flex flex-col flex-1 gap-4 px-5 pb-5">
-        <p className="pr-2 pl-9 font-regular text-brown2 whitespace-break-spaces">
+        <p className="pr-2 pl-[38px] font-regular text-brown2 whitespace-break-spaces">
           {data.search_memos_response?.processed_message}
         </p>
         {data.search_memos_response?.memos?.length !== 0 && (
@@ -29,6 +40,11 @@ const AccordionContent = ({
               <UneditableMemo key={memo.id} memo={memo} />
             ))}
           </MemosList>
+        )}
+        {isOpen && (
+          <p className="ml-auto text-sm font-regular text-brown2 flex-shrink-0">
+            {formatDate(data.created_at)}
+          </p>
         )}
       </div>
     </div>
