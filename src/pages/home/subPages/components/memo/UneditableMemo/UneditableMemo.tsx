6cbcd340d727688fc90459606187390cd37458c2
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-22 13:27:51 +0900

    design: 메모에 이미지가 있을때, 정사각형으로 보이게 수정

--- src/pages/home/subPages/components/memo/UneditableMemo/UneditableMemo.tsx
+++ src/pages/home/subPages/components/memo/UneditableMemo/UneditableMemo.tsx
@@ -5,6 +5,7 @@ import { MemoText } from 'pages/home/subPages/components';
 import { Memo } from 'pages/home/subPages/interfaces';
 import { MemoFooter } from './MemoFooter';
 import { MemoHeader } from './MemoHeader';
+import { ImageBlur } from './ImageBlur';
 
 const UneditableMemo = ({
   memo,
@@ -29,8 +30,14 @@ const UneditableMemo = ({
   const mouseDownTime = useRef<number | null>(null);
 
   const haveImageUrl = memo.image_urls && memo.image_urls.length > 0;
-  const getColor = (defaultColor: string, colorOnImage: string) =>
-    haveImageUrl ? colorOnImage : defaultColor;
+  const getStyleByImagePresence = (
+    defaultStyle: string,
+    styleByImagePresence: string
+  ) => (haveImageUrl ? styleByImagePresence : defaultStyle);
+  const getBackgroundImageStyleByImagePresence = (
+    imageUrl: string | undefined,
+    defaultBackground: string
+  ) => (haveImageUrl ? `url(${imageUrl})` : defaultBackground);
 
   const handleDeleteMemo = async () => {
     softDeleteMemo && softDeleteMemo(memo.id);
@@ -63,32 +70,32 @@ const UneditableMemo = ({
 
   return (
     <div
-      className={`relative flex p-4 min-h-[115px] flex-col gap-[0.88rem] rounded-2xl overflow-hidden
+      className={`relative flex p-4 min-h-[115px] h-auto flex-col gap-[0.88rem] rounded-2xl overflow-hidden
         ${border ? 'border border-black border-opacity-10 bg-clip-padding' : ''} 
-        ${shadow ? 'shadow-custom' : ''} 
-        ${getColor('bg-white', 'bg-cover bg-center')}
+        ${shadow ? 'shadow-custom' : ''} ${getStyleByImagePresence('bg-white', 'bg-cover bg-center')}
+        ${getStyleByImagePresence('', 'aspect-[1/1]')}
       `}
       style={{
-        backgroundImage:
-          memo.image_urls && memo.image_urls.length > 0
-            ? `url(${memo.image_urls[0]})`
-            : 'none',
+        backgroundImage: getBackgroundImageStyleByImagePresence(
+          memo.image_urls?.[0],
+          'none'
+        ),
       }}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onClick={handleClick}
     >
-      {memo.image_urls && memo.image_urls.length > 0 && <ImageBlur />}
+      {haveImageUrl && <ImageBlur />}
 
-      <div className={`flex flex-col gap-2 relative z-10`}>
+      <div className={`flex flex-col h-full gap-2 relative z-10`}>
         <MemoHeader tags={tags} />
         <MemoText
-          textColor={getColor('#111111', 'white')}
+          textColor={getStyleByImagePresence('#111111', 'white')}
           message={message}
           setMessage={setMessage}
         />
         <MemoFooter
-          textColor={getColor('gray2', 'white')}
+          textColor={getStyleByImagePresence('gray2', 'white')}
           updatedAt={memo.updated_at}
           dateFormat={t('memo.dateFormat')}
           handleDeleteMemo={handleDeleteMemo}
@@ -97,17 +104,5 @@ const UneditableMemo = ({
     </div>
   );
 };
-
-const ImageBlur = () => (
-  <div
-    className="absolute inset-0
-      bg-[rgba(38,38,38,0.45)] 
-      backdrop-blur-[1.5px] rounded-2xl overflow-hidden"
-    style={{
-      strokeWidth: '1px',
-      stroke: 'rgba(0, 0, 0, 0.8)',
-    }}
-  />
-);
 
 export default UneditableMemo;
