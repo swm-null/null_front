42211d2a08ee0988d5d1d112695ea2ee4e8d011f
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-23 17:28:56 +0900

    feat: dashboard section list 내부의 클릭은 다 드래그에 클릭 안되게 해야하므로 hook으로 클릭 따로 구현

--- src/pages/home/subPages/hooks/index.ts
+++ src/pages/home/subPages/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as usePressEnterFetch } from './usePressEnterFetch';
 export { default as useIntersectionObserver } from './useIntersectionObserver';
+export { default as useClickWithoutDrag } from './useClickWithoutDrag';
