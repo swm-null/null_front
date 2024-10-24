a90fd1757be42d523ee32381ece19bd06ee0690b
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-24 18:08:42 +0900

    fix: [NULL-506] Login 창에서 액세스 토큰이 있으면 바로 메인 창으로 이동하게 수정

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { LoginSignupButton, CustomInput, HiddenInput } from 'pages/components';
@@ -8,6 +8,8 @@ import { isLoginResponse, login } from 'api';
 const Login = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+
+  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -26,15 +28,24 @@ const Login = () => {
     navigate('/signup');
   };
 
+  useEffect(() => {
+    const accessToken = Cookies.get('access_token');
+    if (accessToken) {
+      navigate('/');
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  if (loading) {
+    return <></>;
+  }
+
   return (
     <div className="bg-custom-gradient-basic flex justify-center items-center h-screen py-8">
       <div className="flex flex-col bg-[#FFF6E3CC] p-8 shadow-custom w-full max-w-lg rounded-2xl gap-6">
         <div className="flex flex-col gap-2">
-          <CustomInput
-            label={t('login.email')}
-            value={email}
-            setValue={setEmail}
-          />
+          <CustomInput label={t('login.email')} value={email} setValue={setEmail} />
           <HiddenInput
             label={t('login.password')}
             value={password}
@@ -54,10 +65,7 @@ const Login = () => {
         </div>
 
         <div className="flex gap-2 flex-col">
-          <LoginSignupButton
-            label={t('login.loginButton')}
-            onClick={handleLogin}
-          />
+          <LoginSignupButton label={t('login.loginButton')} onClick={handleLogin} />
           <LoginSignupButton
             label={t('login.signupButton')}
             onClick={handleSignup}
