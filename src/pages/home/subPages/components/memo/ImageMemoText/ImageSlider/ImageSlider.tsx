2745c4a506cd26f8577192f1d0bfef90755c57c2
Minji Yun <gee2450@kookmin.ac.kr>
2024-10-29 20:25:24 +0900

    feat: 슬라이드의 마지막에 이미지 추가 버튼 추가하고 클릭 구현

--- src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/ImageSlider.tsx
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/ImageSlider.tsx
@@ -1,39 +1,69 @@
+import { AddIcon } from 'assets/icons';
+import { useEffect, useState } from 'react';
 import Flickity from 'react-flickity-component';
 
 const ImageSlider = ({
-  image_urls,
+  imageUrls,
+  editable = true,
 }: {
-  image_urls: string[] | null;
-  // FIXME: 이미지 편집 가능하게 나중에 추가
+  imageUrls: string[] | null;
   editable?: boolean;
 }) => {
+  const [flickityInstance, setFlickityInstance] = useState<Flickity | null>(null);
+
+  useEffect(() => {
+    if (flickityInstance) {
+      const handleStaticClick = (_: any, __: any, ___: any, cellIndex: number) => {
+        if (cellIndex !== undefined) {
+          if (cellIndex === imageUrls?.length) {
+            alert('이미지 추가 버튼 클릭됨');
+          }
+        }
+      };
+
+      flickityInstance.on('staticClick', handleStaticClick);
+
+      return () => {
+        flickityInstance.off('staticClick', handleStaticClick);
+      };
+    }
+  }, [flickityInstance, imageUrls, editable]); // flickityInstance를 의존성에 추가
+
   const flickityOptions = {
     prevNextButtons: false,
-    pageDots: image_urls && image_urls.length > 1 ? true : undefined,
-    wrapAround: true,
+    pageDots: imageUrls ? true : undefined,
     setGallerySize: false,
   };
 
-  if (image_urls?.length) {
+  if (imageUrls?.length || editable) {
     return (
-      <Flickity
-        className="carousel xs:w-60 w-full max-w-72 rounded-2xl overflow-hidden aspect-square"
-        elementType="div"
-        options={flickityOptions}
-        static
-      >
-        {image_urls.map((url, index) => (
-          <img
-            src={url}
-            key={index}
-            alt={`Memo Image ${index + 1}`}
-            className="carousel-cell object-cover w-full h-full xsm:w-60"
-          />
-        ))}
-      </Flickity>
+      <div className="xs:w-60 w-full max-w-72 rounded-2xl overflow-hidden">
+        <Flickity
+          flickityRef={(instance) => setFlickityInstance(instance)}
+          elementType="div"
+          className="carousel w-full h-full aspect-square"
+          options={flickityOptions}
+          static
+        >
+          {imageUrls?.map((url, index) => (
+            <img
+              src={url}
+              key={index}
+              alt={`Memo Image ${index + 1}`}
+              className="carousel-cell object-cover w-full h-full xsm:w-60"
+            />
+          ))}
+
+          {editable && (
+            <div className="carousel-cell flex items-center justify-center w-full h-full bg-gray-200 xsm:w-60">
+              <AddIcon className="w-10 h-10" />
+            </div>
+          )}
+        </Flickity>
+      </div>
     );
   } else {
-    <></>;
+    return <></>;
   }
 };
 
