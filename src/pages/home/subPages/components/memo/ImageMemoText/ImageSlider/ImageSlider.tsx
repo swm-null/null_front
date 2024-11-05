efa2faf7d6597d442f86f2abdb2b34c0adf2c272
Minji Yun <gee2450@kookmin.ac.kr>
2024-11-05 20:35:31 +0900

    style: 이미지 슬라이더 부분 코드 정리

--- src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/ImageSlider.tsx
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/ImageSlider.tsx
@@ -1,11 +1,8 @@
-import { useContext, useMemo, useState, useEffect } from 'react';
-import Flickity from 'react-flickity-component';
+import { useContext, useMemo, useState } from 'react';
 import { ImageListContext } from 'utils';
-import { ImageFileInput } from 'pages/home/subPages/components/utils';
-import { AddIcon } from 'assets/icons';
-import 'yet-another-react-lightbox/styles.css';
-import ImageLightbox from './ImageLightbox';
-import ImageItem from './ImageItem';
+import { ImageLightbox } from './ImageLightbox';
+import { FlickityWrapper } from './FlickityWrapper';
+import { AddItem, ImageItem } from './item';
 
 const ImageSlider = ({
   imageUrls,
@@ -19,91 +16,52 @@ const ImageSlider = ({
   const { images, removeImage, handleAddImageButtonClick, handleImageFilesChange } =
     useContext(ImageListContext);
 
-  const [flickityInstance, setFlickityInstance] = useState<Flickity | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [photoIndex, setPhotoIndex] = useState(0);
 
   const allImages = useMemo(() => {
     const objectUrls = images.map((img) => URL.createObjectURL(img));
-
     return [...(imageUrls || []), ...objectUrls];
   }, [imageUrls, images]);
 
-  const isNoImages = () => imageUrls?.length + images.length === 0;
-  const isLastImageIndex = (cellIndex: number) => cellIndex === allImages?.length;
+  const isNoImages = () => allImages.length === 0;
 
-  useEffect(() => {
-    if (!flickityInstance) return;
+  const handleRemoveClick = (index: number) => {
+    if (index < imageUrls?.length) {
+      removeImageUrl?.(index);
+    } else {
+      removeImage(index - imageUrls?.length);
+    }
+  };
 
-    const handleStaticClick = (event: any, _: any, __: any, cellIndex: number) => {
-      const removeButton = (event.target as HTMLElement).closest('.remove-button');
-      if (removeButton instanceof HTMLElement) {
-        if (cellIndex < imageUrls?.length) {
-          removeImageUrl && removeImageUrl(cellIndex);
-        } else {
-          removeImage(cellIndex - imageUrls?.length);
-        }
-        event.preventDefault();
-        return;
-      }
-
-      if (cellIndex && isLastImageIndex(cellIndex)) {
-        handleAddImageButtonClick();
-      } else {
-        setPhotoIndex(cellIndex);
-        setIsOpen(true);
-      }
-    };
-
-    flickityInstance.on('staticClick', handleStaticClick);
-
-    return () => {
-      flickityInstance.off('staticClick', handleStaticClick);
-    };
-  }, [flickityInstance, imageUrls, images, editable]);
+  const handleImageClick = (index: number) => {
+    setPhotoIndex(index);
+    setIsOpen(true);
+  };
 
   if (isNoImages()) {
     return <></>;
   }
 
-  const flickityOptions = {
-    prevNextButtons: false,
-    pageDots: imageUrls ? true : undefined,
-    setGallerySize: false,
-  };
-
   return (
     <div className="xs:w-60 w-full max-w-72 rounded-2xl overflow-hidden">
-      <Flickity
-        elementType="div"
-        flickityRef={(instance) => setFlickityInstance(instance)}
-        className="carousel w-full h-full aspect-square"
-        options={flickityOptions}
+      <FlickityWrapper
+        showPageDots={!!imageUrls}
+        onImageClick={handleImageClick}
+        onRemoveClick={editable ? handleRemoveClick : null}
+        onAddClick={editable ? handleAddImageButtonClick : null}
+        totalImageCount={allImages.length}
       >
-        {imageUrls?.map((url, index) => (
+        {allImages.map((image, index) => (
           <ImageItem
             key={`origin-${index}`}
-            image={url}
-            index={index}
-            editable={editable}
-          />
-        ))}
-        {images?.map((image, index) => (
-          <ImageItem
-            key={`new-${index}`}
             image={image}
             index={index}
             editable={editable}
           />
         ))}
-        {editable && (
-          <div className="carousel-cell flex items-center justify-center w-full h-full bg-gray-200 xsm:w-60">
-            <ImageFileInput handleImageFileChange={handleImageFilesChange}>
-              <AddIcon className="w-10 h-10" />
-            </ImageFileInput>
-          </div>
-        )}
-      </Flickity>
+        {editable && <AddItem handleImageFilesChange={handleImageFilesChange} />}
+      </FlickityWrapper>
 
       <ImageLightbox
         isOpen={isOpen}
---
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/item/index.ts
@@ -0,0 +1,2 @@
+export { default as ImageItem } from './ImageItem';
+export { default as AddItem } from './AddItem';
