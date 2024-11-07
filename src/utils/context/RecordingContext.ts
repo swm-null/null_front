e356061d46729b2fef98ebe5f9d3633becf489de
Minji Yun <gee2450@kookmin.ac.kr>
2024-11-08 05:01:04 +0900

    feat: 저장 버튼을 누르면 audioBlob에 오디오 저장하게 수정

--- src/utils/context/RecordingContext.ts
+++ src/utils/context/RecordingContext.ts
@@ -3,16 +3,19 @@ import { createContext } from 'react';
 export type RecordingModalState = {
   open: boolean;
   onClose: () => void;
+  onSend: (blob: Blob) => void;
 };
 
 type RecordingContextType = {
   recordingModal: RecordingModalState | null;
   openRecordingModal: () => void;
   closeRecordingModal: () => void;
+  audioBlob: Blob | null;
 };
 
 export const RecordingContext = createContext<RecordingContextType>({
   recordingModal: null,
   openRecordingModal: () => {},
   closeRecordingModal: () => {},
+  audioBlob: null,
 });
--- src/utils/provider/RecordingProvider.tsx
+++ src/utils/provider/RecordingProvider.tsx
@@ -5,16 +5,23 @@ const RecordingProvider = ({ children }: { children: ReactNode }) => {
   const [recordingModal, setRecordingModal] = useState<RecordingModalState | null>(
     null
   );
+  const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+
+  const closeRecordingModal = useCallback(() => {
+    setRecordingModal(null);
+  }, []);
+
+  const handleSaveAudio = useCallback((blob: Blob) => {
+    setAudioBlob(blob);
+    closeRecordingModal();
+  }, []);
 
   const openRecordingModal = useCallback(() => {
     setRecordingModal({
       open: true,
       onClose: () => closeRecordingModal(),
+      onSend: handleSaveAudio,
     });
-  }, []);
-
-  const closeRecordingModal = useCallback(() => {
-    setRecordingModal(null);
   }, []);
 
   return (
@@ -23,6 +30,7 @@ const RecordingProvider = ({ children }: { children: ReactNode }) => {
         recordingModal,
         openRecordingModal,
         closeRecordingModal,
+        audioBlob,
       }}
     >
       {children}
