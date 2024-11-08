9ac69c3ed0d1f868b8b9a6bb963c63cb74c14f9a
Minji Yun <gee2450@kookmin.ac.kr>
2024-11-09 04:20:59 +0900

    feat: 하이퍼링크 생기게 수정

--- src/pages/home/subPages/components/memo/ImageMemoText/MemoText/MemoText.tsx
+++ src/pages/home/subPages/components/memo/ImageMemoText/MemoText/MemoText.tsx
@@ -1,41 +1,70 @@
-import { ChangeEvent, useEffect, useRef } from 'react';
+import {
+  ChangeEvent,
+  FocusEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { TextareaAutosize } from '@mui/material';
 
 const MemoText = ({
   message,
   textColor = '#111111',
   setMessage,
-  handleBlur,
   editable = false,
 }: {
   message: string;
   textColor?: string;
   setMessage?: (newMessage: string) => void;
-  handleBlur?: () => void;
   editable?: boolean;
 }) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [isBlurred, setIsBlurred] = useState(true);
+
+  const convertToHyperlinks = (text: string) => {
+    const urlPattern = /(https?:\/\/[^\s]+)/g;
+    return text.replace(
+      urlPattern,
+      '<a class="underline" href="$1" target="_blank">$1</a>'
+    );
+  };
+
+  const handleFocus = useCallback(() => {
+    if (textareaRef.current) {
+      setIsBlurred(true);
+    }
+  }, [textareaRef.current]);
+
+  const handleBlur = (e: FocusEvent<HTMLTextAreaElement>) => {
+    e.target.blur();
+    setIsBlurred(false);
+  };
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setMessage && setMessage(e.target.value);
   };
 
   useEffect(() => {
-    if (textareaRef.current && editable) {
-      textareaRef.current.focus();
-    }
-  }, [textareaRef.current]);
+    textareaRef.current?.focus();
+  }, [isBlurred]);
 
   return (
     <div className="flex flex-1 w-full">
       <TextareaAutosize
         ref={textareaRef}
-        className={`w-full bg-transparent focus:outline-none resize-none font-regular text-[15px]`}
+        className={`w-full bg-transparent focus:outline-none resize-none font-regular text-[15px] ${isBlurred && editable ? '' : 'hidden'}`}
         style={{ color: textColor }}
         value={message}
         onChange={handleChange}
         onBlur={handleBlur}
-        readOnly={!editable}
+      />
+      <div
+        className={`w-full bg-transparent font-regular text-[15px] whitespace-break-spaces 
+          [&_a]:pointer-events-auto ${!isBlurred || !editable ? '' : 'hidden'}`}
+        style={{ color: textColor }}
+        dangerouslySetInnerHTML={{ __html: convertToHyperlinks(message) }}
+        onClick={handleFocus}
       />
     </div>
   );
