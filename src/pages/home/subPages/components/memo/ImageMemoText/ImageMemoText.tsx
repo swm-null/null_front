cdba4f07bc1b858fe62dfb18a6b784ccf3dd649a
Minji Yun <gee2450@kookmin.ac.kr>
2024-11-13 19:17:22 +0900

    fix: imageUrls 없을때, 텍스트 왼쪽에 여백이 생기는 오류 수정

--- src/pages/home/subPages/components/memo/ImageMemoText/ImageMemoText.tsx
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageMemoText.tsx
@@ -37,7 +37,7 @@ const ImageMemoText = ({
       >
         {message || editable ? (
           <div className="w-full overflow-hidden">
-            {imageUrls && (
+            {imageUrls.length !== 0 && (
               <div className="xsm:float-left mr-4 mb-1">
                 <ImageSlider
                   imageUrls={imageUrls}
