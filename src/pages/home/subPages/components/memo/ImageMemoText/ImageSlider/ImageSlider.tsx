09a7655da1724a00010e06f0aede0713c8c10b4e
Minji Yun <gee2450@kookmin.ac.kr>
2024-11-13 19:16:13 +0900

    [NULL-622] Feature/image slider dot (#99)
    * feat: slider에 dot 직접 새로 구현
    
    * feat: page dots 컴포넌트 생성
    
    * design: icon의 선의 두께가 얇아서 add icon이 잘 안 보여서 색을 더 진하게 수정

--- src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/ImageSlider.tsx
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/ImageSlider.tsx
@@ -3,6 +3,7 @@ import { ImageListContext } from 'utils';
 import { ImageLightbox } from './ImageLightbox';
 import { FlickityWrapper } from './FlickityWrapper';
 import { AddItem, ImageItem } from './item';
+import { PageDots } from './PageDots';
 
 const ImageSlider = ({
   imageUrls,
@@ -18,6 +19,9 @@ const ImageSlider = ({
 
   const [isOpen, setIsOpen] = useState(false);
   const [photoIndex, setPhotoIndex] = useState(0);
+
+  const showPageDots =
+    (imageUrls.length + images.length >= 1 && editable) || imageUrls.length >= 2;
 
   const allImages = useMemo(() => {
     const objectUrls = editable ? images.map((img) => URL.createObjectURL(img)) : [];
@@ -44,28 +48,35 @@ const ImageSlider = ({
   }
 
   return (
-    <div className="xs:w-60 w-full max-w-72 rounded-2xl overflow-hidden">
-      <FlickityWrapper
-        showPageDots={
-          (imageUrls.length + images.length >= 1 && editable) ||
-          imageUrls.length >= 2
-        }
-        onImageClick={handleImageClick}
-        onRemoveClick={editable ? handleRemoveClick : null}
-        onAddClick={editable ? handleAddImageButtonClick : null}
-        totalImageCount={allImages.length}
-      >
-        {allImages.map((image, index) => (
-          <ImageItem
-            key={`origin-${index}`}
-            image={image}
-            index={index}
-            editable={editable}
-          />
-        ))}
-        {editable && <AddItem handleImageFilesChange={handleImageFilesChange} />}
-      </FlickityWrapper>
-
+    <>
+      <div className="xs:w-60 w-full max-w-72 rounded-2xl overflow-hidden">
+        <FlickityWrapper
+          onImageClick={handleImageClick}
+          onRemoveClick={editable ? handleRemoveClick : null}
+          onAddClick={editable ? handleAddImageButtonClick : null}
+          totalImageCount={allImages.length}
+          onChange={setPhotoIndex}
+          currentIndex={photoIndex}
+        >
+          {allImages.map((image, index) => (
+            <ImageItem
+              key={`origin-${index}`}
+              image={image}
+              index={index}
+              editable={editable}
+            />
+          ))}
+          {editable && <AddItem handleImageFilesChange={handleImageFilesChange} />}
+        </FlickityWrapper>
+      </div>
+      {showPageDots && (
+        <PageDots
+          allImages={allImages}
+          photoIndex={photoIndex}
+          setPhotoIndex={setPhotoIndex}
+          editable={editable}
+        />
+      )}
       <ImageLightbox
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
@@ -73,7 +84,7 @@ const ImageSlider = ({
         setPhotoIndex={setPhotoIndex}
         images={allImages}
       />
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/PageDots/PageDots.tsx
@@ -0,0 +1,45 @@
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+
+interface PageDotsProps {
+  allImages: string[];
+  photoIndex: number;
+  setPhotoIndex: (index: number) => void;
+  editable?: boolean;
+}
+
+const PageDots = ({
+  allImages,
+  photoIndex,
+  setPhotoIndex,
+  editable,
+}: PageDotsProps) => {
+  return (
+    <div className="flex gap-1 justify-center mt-2 h-4">
+      {Array.from({ length: allImages.length }).map((_, index) => (
+        <button
+          type="button"
+          className="p-1"
+          key={`dot-${index}`}
+          onClick={() => setPhotoIndex(index)}
+        >
+          <div
+            className={`w-1.5 h-1.5 rounded-full p-1 self-center ${
+              index === photoIndex ? 'bg-black' : 'bg-gray1'
+            }`}
+          />
+        </button>
+      ))}
+      {editable && (
+        <AddOutlinedIcon
+          sx={{ width: 16, height: 16, padding: 0, margin: 0 }}
+          className={`cursor-pointer ${
+            photoIndex === allImages.length ? 'text-black' : 'text-gray2'
+          }`}
+          onClick={() => setPhotoIndex(allImages.length)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PageDots;
