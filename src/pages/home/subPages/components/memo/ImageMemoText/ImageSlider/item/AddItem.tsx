685fda14109611c6febcacca88bdf384853211b5
Minji Yun <gee2450@kookmin.ac.kr>
2024-11-15 01:30:46 +0900

    design: 이미지 슬라이더 색깔 수정

--- src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/item/AddItem.tsx
+++ src/pages/home/subPages/components/memo/ImageMemoText/ImageSlider/item/AddItem.tsx
@@ -8,9 +8,9 @@ const AddItem = ({
   handleImageFilesChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
-    <div className="carousel-cell flex items-center justify-center w-full h-full bg-gray-200 xsm:w-60">
+    <div className="carousel-cell flex items-center justify-center w-full h-full bg-[#E6DDCF] xsm:w-60">
       <ImageFileInput handleImageFileChange={handleImageFilesChange}>
-        <AddIcon className="w-10 h-10" />
+        <AddIcon className="w-10 h-10 text-brown2" />
       </ImageFileInput>
     </div>
   );
